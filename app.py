5225fa00e9566624631d9f91f690a283e36a2046
nicbencini <nicbencini@gmail.com>
2025-05-01 23:44:18 +0100

    update: created route for viewing file contents

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-from cms import create_app
 from flask import( 
     Flask,
     render_template,
@@ -8,16 +7,29 @@ from flask import(
 import os
 from pathlib import Path
 
-app = create_app()
+app = Flask(__name__)
 data_folder = Path("cms/data/")
+root = os.path.abspath(os.path.dirname(__file__))
+data_dir = os.path.join(root, "cms", "data")
 
 @app.route('/')
 def index():
 
-    file_names = [name for name in os.listdir(data_folder)]
-    return render_template('index.html', file_names=file_names)
+    
+    
+    files = [os.path.basename(path) for path in os.listdir(data_dir)]
+    return render_template('index.html', file_names=files)
 
+
+@app.route("/<file>")
+def show_file(file):
+
+    file_path = f'{data_dir}/{file}'
+
+    with open(file_path) as f:
+        file_data = f.readlines()
+
+    return render_template('view_file.html', file_data=file_data)
 
 if __name__ == "__main__":
-    app.run(port=5003)
-
+    app.run(debug = True, port=5003)
--- cms/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from cms import create_app
-
-app = create_app()
-
-@app.route('/')
-def index():
-    return 'Hello'
