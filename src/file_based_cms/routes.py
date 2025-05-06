19b75060eec0bb6e6b05022e77cf4906e9636b05
nicbencini <nicbencini@gmail.com>
2025-05-07 00:03:54 +0100

    update: added create new file and login

--- src/file_based_cms/routes.py
+++ src/file_based_cms/routes.py
@@ -30,15 +30,19 @@ def load_file_names():
     g.data_dir = data_dir
     g.file_names = os.listdir(data_dir)
 
+    
+
+
 @app.route('/')
 def index():
 
-    file_names = [os.path.basename(path) for path in g.file_names]
-    return render_template('index.html', file_names=file_names)
+
+    return render_template('index.html', file_names=g.file_names)
 
 
 @app.route("/<file>")
 def show_file(file):
+
 
     if file not in g.file_names:
         flash('The file could not be found.', 'warning')
@@ -58,6 +62,7 @@ def show_file(file):
 @app.route("/<file>", methods=['POST'])
 def save_file(file):
 
+
     file_path = os.path.join(g.data_dir, file)
 
     content = request.form['file_contents']
@@ -71,6 +76,7 @@ def save_file(file):
 @app.route("/<file>/edit")
 def edit_file(file):
 
+
     file_path = os.path.join(g.data_dir, file)
     
     if os.path.isfile(file_path):
@@ -80,3 +86,74 @@ def edit_file(file):
     else:
         flash(f"{file} does not exist.")
         return redirect(url_for('index'))
+
+@app.route("/new")
+def new_file():
+    
+    return render_template('new_file.html')
+
+
+@app.route("/new", methods=['POST'])
+def create_file():
+
+
+    new_file_name = request.form['file_name']
+
+    if new_file_name in g.file_names:
+        flash(f"'{new_file_name}' already exists.")
+    elif not new_file_name.endswith('.txt') or not new_file_name.endswith('.md'):
+        flash(f"Please enter a valid file extension '.txt' or '.md'")
+    else:
+        create_document(new_file_name)
+
+    return redirect(url_for('index'))
+
+@app.route("/<file>/delete")
+def delete_file(file):
+
+    file_path = os.path.join(g.data_dir, file)
+    
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        flash(f"'{file}' has been deleted.")
+        return redirect(url_for('index'))
+    else:
+        flash(f"{file} does not exist.")
+        return redirect(url_for('index'))
+
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
+@app.route("/signin", methods=['POST'])
+def check_credentials():
+
+    username = request.form['username']
+    password = request.form['password']
+
+    if username == "admin" and password == "secret":
+        session['username'] = username
+        flash("Welcome!")
+        return redirect(url_for('index'))
+    else:
+        flash("Invalid credentials")
+        return render_template('signin.html'), 422
+
+
+@app.route("/signout")
+def signout():
+
+    session['username'] = ''
+    session['password'] = ''
+
+    return redirect(url_for('index'))
+    
+def create_document(name, content=""):
+    with open(os.path.join(g.data_dir, name), 'w') as file:
+        file.write(content)
+
+def check_creds(username, password):
+    if username == 'admin' and password == 'secret':
+        return True
+    return False
+
